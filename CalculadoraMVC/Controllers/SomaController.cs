c6ca795c2cabf8dc96408aa3df132b644860e263
Saulo Carvalho <saulo.carv@hotmail.com>
2018-06-12 16:31:22 -0300

    Só atualizando codigo

---
+++ CalculadoraMVC/Controllers/SomaController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CalculadoraMVC.Controllers
+{
+    public class SomaController : Controller
+    {
+        // GET: Soma
+        public string Index(int numero1, int numero2)
+        {
+            int somatorio = numero1 + numero2;
+            return "ID =" + id + "<br /> Name=" + Request.QueryString["name"].ToString();
+        }
+    }
+}
