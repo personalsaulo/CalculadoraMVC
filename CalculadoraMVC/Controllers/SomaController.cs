44b88d05c3894c43c172fe45c65d2fdce481f459
Saulo Carvalho <saulo.carv@hotmail.com>
2018-06-12 21:11:05 -0300

    Criei uma view para a soma.

--- CalculadoraMVC/Controllers/SomaController.cs
+++ CalculadoraMVC/Controllers/SomaController.cs
@@ -9,10 +9,11 @@ namespace CalculadoraMVC.Controllers
     public class SomaController : Controller
     {
         // GET: Soma
-        public string Index(int numero1, int numero2)
+        public ActionResult Addition(int numero1, int numero2)
         {
-            int somatorio = numero1 + numero2;
-            return "ID =" + id + "<br /> Name=" + Request.QueryString["name"].ToString();
+            return View();
         }
+
+        
     }
 }
